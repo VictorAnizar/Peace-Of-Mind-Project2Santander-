7931c25c422851cc51aa370f17d0347e68802f7f
VictorAnizar <victoranizarmorales@gmail.com>
2021-09-19 19:46:46 -0500

    instalando mongoose y configurandolo en el appJS

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 //se parsean los bodys a json
 app.use(bodyParser.json());
-
+//Configuración de la Base de datos
+//primero decimos que usaremos mongoose
+const mongoose=require('mongoose');
+mongoose.connect();
+//para que se generen alertas y errores de una forma verbosa
+mongoose.set("debug", true);
+//usamos los modelos
+require('./models/Usuario');
+require('./models/Mascota');
+require('./models/Solicitud');
+//configuracion de las rutas
 app.use('/v1', require('./routes'));
-
-
 //decimos cómo/dónde se va a ejecutar la app
 //definimos el puerto 
 const PORT = 3464;
