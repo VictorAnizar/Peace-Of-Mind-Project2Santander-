c2b93260cb4d23da0618c5223a3f1e7b07a1bc76
VictorAnizar <victoranizarmorales@gmail.com>
2021-09-21 19:12:58 -0500

    agregando modelo de recurso

--- models/Recurso.js
+++ models/Recurso.js
@@ -1,10 +1,20 @@
-class Recurso{
-    constructor(idRecurso, tipo, nombre, link, idEnfermedad){
-        this.idRecurso=idRecurso;
-        this.tipo=tipo;
-        this.nombre=nombre;
-        this.link=link;
-        this.idEnfermedad=idEnfermedad;
+const mongoose = require('mongoose');
+
+const RecursoScheme = new mongoose.Schema({
+    tipo: {type: String, required: true},
+    nombre: {type: String, required: true},
+    link: {type: String, required: true},
+    idEnfermedad: { type: mongoose.Schema.Types.ObjectId, ref: "Enfermedad" }
+})
+
+RecursoScheme.methods.publicData = ()=>{
+    return {
+        id: this.id,
+        tipo: this.tipo,
+        nombre: this.nombre,
+        link: this.link,
+        idEnfermedad: this.idEnfermedad
     }
 }
-module.exports=Recurso
+
+mongoose.model("recursos", RecursoScheme);
