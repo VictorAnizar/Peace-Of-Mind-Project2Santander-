e22c06496ee047a68303c603b2bb9491b088ef3f
VictorAnizar <victoranizarmorales@gmail.com>
2021-09-23 09:33:02 -0500

    Terminando todo el CRUD de las entidades

--- controllers/articulos.js
+++ controllers/articulos.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Articulo = mongoose.model("Articulo")
 function crearArticulo() {
   // Instanciaremos un nuevo Articulo utilizando la clase Articulo
-  
+  let articulo = new Articulo(req.params.id);
+  articulo.save()
+  .then(art=>{
+    res.status(200).send("Registro creado")
+  })
+  .catch(next);
 }
 
 function obtenerArticulos(req, res, next) {
@@ -47,12 +52,45 @@ function obtenerArticuloPorPropiedad(req, res){
 
 }
 
-function modificarArticulo() {
- 
+function modificarArticulo(req, res, next) {
+  Articulo.findById(req.params.id)
+  .then(art => {
+    //Si es Undefined
+    if (!art) {
+      return res.sendStatus(401);
+    }
+    //Si sí existe, obtenemos lo que nos manda el usuario
+    let nuevaInfo = req.body;
+    //Si se desea cambiar el titulo
+    if (typeof nuevaInfo.titulo !== "undefined") {
+      art.titulo = nuevaInfo.titulo
+    }
+    //Si se desea cambiar el texto
+    if (typeof nuevaInfo.texto !== "undefined") {
+      art.texto = nuevaInfo.texto
+    }
+    //Si se desea cambiar el referencias
+    if (typeof nuevaInfo.referencias !== "undefined") {
+      art.referencias = nuevaInfo.referencias
+    }
+   
+    art.save()
+      .then(
+        //se manda a la BD en forma de JSON
+        updated => res.status(200).json(updated.publicData())
+      )
+      .catch(next);
+
+  })
+  .catch(next);
 }
 
-function eliminarArticulo() {
-  
+function eliminarArticulo(req, res, next) {
+  Articulo.findByIdAndDelete({_id:req.params.id})
+  .then(art=>{
+    res.send("Registro eliminado");
+  }) 
+  .catch(next); 
 }
 
 // exportamos las funciones definidas
